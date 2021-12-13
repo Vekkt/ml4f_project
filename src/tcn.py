0e6df191b81deb42ec22444b31c4b5becfc61dc7
vekkt <francois.burguet@epfl.ch>
2021-12-13 16:50:11 +0100

    first draft for tcn

---
+++ src/tcn.py
@@ -0,0 +1,53 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers, Sequential
+
+
+class TemporalBlockModule(layers.Layer):
+    def __init__(self, input_size, hidden_size, output_size, dilation, kernel_size):
+        self.block = Sequential()
+        self.block.add(layers.Conv1D(hidden_size, kernel_size,
+                              dilation_rate=dilation,
+                              padding='causal'))
+        self.block.add(layers.PReLU())
+        self.block.add(layers.Conv1D(output_size, kernel_size,
+                              dilation_rate=dilation,
+                              padding='causal'))
+        self.block.add(layers.PReLU())
+
+
+    def call(self, inputs):
+        return self.block(inputs), inputs
+
+
+class TCN(layers.Layer):
+    def __init__(self, input_size, hidden_size, output_size, dilation, kernel_size):
+        self.modules = [TemporalBlockModule(
+            input_size, hidden_size, output_size, 1, 1)]
+
+        for i in range(6):
+            self.modules.append(TemporalBlockModule(
+                input_size, hidden_size, output_size, 2, 2**i))
+
+        self.conv = layers.Conv1D(output_size, 1, dilation_rate=1)
+        
+    def call(self, inputs):
+        out_layers = []
+        for temporalBlock in self.modules:
+            out, x = temporalBlock(inputs)
+            out_layers.append(x)
+        
+        out_layers.append(out)
+        return layers.add(out_layers)
+            
+
+
+# def tcn(input_size, hidden_size, output_size, dilation, kernel_size):
+#     modules = [TemporalBlockModule(input_size, hidden_size, output_size, 1, 1)]
+
+#     for i in range(6):
+#         modules.append(TemporalBlockModule(
+#             input_size, hidden_size, output_size, 2, 2**i))
+
+#     conv = layers.Conv1D(output_size, 1, dilation_rate=1)
