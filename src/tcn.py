7b75c3b8ed3ee0b3b08442fd06b16891712b0b40
vekkt <francois.burguet@epfl.ch>
2021-12-15 15:52:33 +0100

    cleaned imports

--- src/tcn.py
+++ src/tcn.py
@@ -1,38 +1,36 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers, Sequential
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Layer, Conv1D, ReLU, add
 
 
-class TemporalBlockModule(layers.Layer):
+class TemporalBlock(Layer):
     def __init__(self, input_size, hidden_size, output_size, dilation, kernel_size):
-        super(TemporalBlockModule, self).__init__()
+        super(TemporalBlock, self).__init__()
         self.block = Sequential()
-        self.block.add(layers.Conv1D(hidden_size, kernel_size,
+        self.block.add(Conv1D(hidden_size, kernel_size,
                                      dilation_rate=dilation,
                                      padding='causal',
                                      input_shape=(None, input_size)))
-        self.block.add(layers.ReLU())  # NEED TO CHANGE THAT TO PRELU
-        self.block.add(layers.Conv1D(output_size, kernel_size,
+        self.block.add(ReLU())  # NEED TO CHANGE THAT TO PRELU
+        self.block.add(Conv1D(output_size, kernel_size,
                                      dilation_rate=dilation,
                                      padding='causal'))
-        self.block.add(layers.ReLU())  # NEED TO CHANGE THAT TO PRELU
+        self.block.add(ReLU())  # NEED TO CHANGE THAT TO PRELU
 
     def call(self, inputs):
         return self.block(inputs)
 
 
-class TCN(layers.Layer):
+class TCN(Layer):
     def __init__(self, input_size, hidden_size, output_size):
         super(TCN, self).__init__()
-        self.modules = [TemporalBlockModule(
+        self.modules = [TemporalBlock(
             input_size, hidden_size, hidden_size, 1, 1)]
 
         for i in range(6):
-            self.modules.append(TemporalBlockModule(
+            self.modules.append(TemporalBlock(
                 hidden_size, hidden_size, hidden_size, 2, 2**i))
 
-        self.conv = layers.Conv1D(output_size, 1, dilation_rate=1)
+        self.conv = Conv1D(output_size, 1, dilation_rate=1)
 
     def call(self, inputs):
         out = inputs
@@ -41,6 +39,6 @@ class TCN(layers.Layer):
             out = temporalBlock(out)
             out_layers.append(out)
 
-        out = layers.add(out_layers)
+        out = add(out_layers)
         out = self.conv(out)
         return out
