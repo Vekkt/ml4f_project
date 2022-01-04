f4d5b13ee2d112955ce8cfb56cee3a839d8395d9
Vekkt <s274495@studenti.polito.it>
2022-01-04 16:00:22 +0100

    fixed errors

--- src/metrics.py
+++ src/metrics.py
@@ -1,29 +1,29 @@
 from scipy.stats import wasserstein_distance
 from numpy.linalg import norm
-from numpy import correlate, vectorize
+from numpy import correlate, array
+
 
 def emd(historical, generated):
     return wasserstein_distance(historical, generated)
 
 
 def acf(r, mode):
-    if mode is 'le':
+    if mode == 'le':
         corr = correlate(r ** 2, r, mode='full')
-    elif mode is 'acf':
+    elif mode == 'acf':
         corr = correlate(r, r, mode='full')
     else:
         raise ValueError("Unknown specified mode for the autocorrelation function.")
-    return corr[corr.size/2:]
+    return corr[corr.size // 2:]
 
 
 def dependence_score(historical, generated_set, mode, func=None):
     if func is None:
-        func = lambda x: x
-    vect_acf = vectorize(lambda r: acf(func(r), mode))
-    
+        def func(x): return x
+
     acf_hist = acf(func(historical), mode)
-    acf_gen = vect_acf(generated_set).mean
-    return norm(acf_hist, acf_gen)
+    acf_gen = array([acf(func(gen), mode) for gen in generated_set])
+    return norm(acf_hist - acf_gen.mean(axis=1))
 
 
 def acf_score(historical, generated_set, func=None):
@@ -34,12 +34,10 @@ def le_score(historical, generated_set):
     return dependence_score(historical, generated_set, 'le')
 
 
-
-from numpy import array
 hist = array([1, 2, 3])
-gen = array([array([1, 3, 5]), array([1, 2, 3]), array([1, 2, 3])])
-print(emd(hist, gen[0]))
+gset = array([array([1, 3, 5]), array([1, 2, 3]), array([1, 2, 3])])
+print(emd(hist, gset[0]))
 print(acf(hist, mode='acf'))
 print(acf(hist, mode='le'))
-print(acf_score(hist, gen))
-print(le_score(hist, gen))
+print(acf_score(hist, gset))
+print(le_score(hist, gset))
