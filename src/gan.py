c8f55e5cc045c17e9f2841271b8cbf5f9cb41a84
vekkt <francois.burguet@epfl.ch>
2022-01-13 18:35:44 +0100

    removed unused rfs param

--- src/gan.py
+++ src/gan.py
@@ -8,18 +8,18 @@ from tcn import TCN
 
 
 class Generator(Layer):
-    def __init__(self, input_size, hidden_size, output_size, rfs=127):
+    def __init__(self, input_size, hidden_size, output_size):
         super(Generator, self).__init__()
-        self.tcn = TCN(input_size, hidden_size, output_size, rfs)
+        self.tcn = TCN(input_size, hidden_size, output_size)
 
     def call(self, inputs):
         return self.tcn(inputs)
 
 
 class Discriminator(Layer):
-    def __init__(self,  input_size, hidden_size, output_size, rfs=127):
+    def __init__(self,  input_size, hidden_size, output_size):
         super(Discriminator, self).__init__()
-        self.tcn = TCN(input_size, hidden_size, output_size, rfs)
+        self.tcn = TCN(input_size, hidden_size, output_size)
 
     def call(self, inputs):
         return sigmoid(self.tcn(inputs))
