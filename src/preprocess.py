75e62fb5a92364ed384ebd7d0f8a3e1ff408d085
Lukachinch <71570126+Lukachinch@users.noreply.github.com>
2022-01-13 19:32:54 +0100

    rolling window

--- src/preprocess.py
+++ src/preprocess.py
@@ -90,6 +90,15 @@ class Gaussianize():
 
 
 
+def rolling_window(array, window_size,freq):
+     shape = (array.shape[0] - window_size + 1, window_size)
+     strides = (array.strides[0],) + array.strides
+     rolled = np.lib.stride_tricks.as_strided(array, shape=shape, strides=strides)
+     return rolled[np.arange(0,shape[0],freq)]
+    
+
+
+
 def main():
      raw_data = pd.read_csv('raw_data.csv')
      logret = np.diff(np.log(raw_data['Close']))
