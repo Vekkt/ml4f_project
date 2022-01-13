d126e56d3217c38e588458d60d8bfe2918a88b15
vekkt <francois.burguet@epfl.ch>
2022-01-13 18:36:24 +0100

    fixed metrics (need opti)

--- src/metrics.py
+++ src/metrics.py
@@ -1,34 +1,30 @@
 from scipy.stats import wasserstein_distance
 from numpy.linalg import norm
-from numpy import correlate, array
+from numpy import array, corrcoef
 
 
 def emd(historical, generated):
     return wasserstein_distance(historical, generated)
 
 
-def acf(r, mode):
-    if mode == 'le':
-        corr = correlate(r ** 2, r, mode='full')
-    elif mode == 'acf':
-        corr = correlate(r, r, mode='full')
-    else:
-        raise ValueError("Unknown specified mode for the autocorrelation function.")
-    return corr[corr.size // 2:]
+def acf(x, s, mode='acf'):
+    xx = x**2 if mode == 'le' else x
+    c0 = corrcoef(x, xx)[0, 1]
+    return array([c0] + [corrcoef(x[:-i], xx[i:])[0, 1] for i in range(1, s)])
 
 
-def dependence_score(historical, generated_set, mode, func=None):
+def dependence_score(historical, generated_set, s, mode, func=None):
     if func is None:
         def func(x): return x
 
-    acf_hist = acf(func(historical), mode)
-    acf_gen = array([acf(func(gen), mode) for gen in generated_set])
-    return norm(acf_hist - acf_gen.mean(axis=1))
+    acf_hist = acf(func(historical), s, mode)
+    acf_gen = array([acf(func(gen), s, mode) for gen in generated_set])
+    return norm(acf_hist - acf_gen.mean(axis=0))
 
 
-def acf_score(historical, generated_set, func=None):
-    return dependence_score(historical, generated_set, 'acf', func)
+def acf_score(historical, generated_set, s, func=None):
+    return dependence_score(historical, generated_set, s, 'acf', func)
 
 
-def le_score(historical, generated_set):
-    return dependence_score(historical, generated_set, 'le')
+def le_score(historical, generated_set, s):
+    return dependence_score(historical, generated_set, s, 'le')
